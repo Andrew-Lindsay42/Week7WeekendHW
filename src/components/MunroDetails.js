dc265f9fe90fc3200d67e30fc00e94474cd98895
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-15 18:09:11 +0000

    Created weatherDetails method to simplify info from API

--- src/components/MunroDetails.js
+++ src/components/MunroDetails.js
@@ -2,29 +2,25 @@ import React from 'react';
 
 const MunroDetails = function ({selectedMunro, munroWeather}){
 
-    console.log('Munro weather:', munroWeather);
-    console.log('Selected Munro:', selectedMunro);
-
     if (selectedMunro !== null && munroWeather !== null){
+
+        const weatherInfo = munroWeather.map((munro, index) => {   
+            return <li key={index}>{munro.date} Temp: {munro.temp}&#8451; Description: {munro.description}<br/></li>
+        })
+        
         return (
             <>
             <div className='selected-munro'>
             <div className='munro-details'>
                 <h4>{selectedMunro.name}</h4>
                 <h5>{selectedMunro.region}</h5>
-                <p>Name meaning: {selectedMunro.meaning}</p>
+                <p>Name: {selectedMunro.meaning}</p>
                 <p>Height: {selectedMunro.height}m</p>
             </div>
             <div className='munro-weather'>
-                {(munroWeather.list[0].dt_txt).slice(-11, -9)}/{(munroWeather.list[0].dt_txt).slice(-14, -12)} {(munroWeather.list[0].dt_txt).slice(-8, -3)}- {munroWeather.list[0].main.temp}&#8451;<br/>
-                {(munroWeather.list[0].weather[0].description)[0].toUpperCase() + (munroWeather.list[0].weather[0].description).substring(1)}<br/>
-                {(munroWeather.list[2].dt_txt).slice(-8, -3)}- {munroWeather.list[2].main.temp}&#8451;<br/>
-                {munroWeather.list[2].weather[0].description}<br/>
-                {(munroWeather.list[6].dt_txt).slice(-8, -3)}- {munroWeather.list[6].main.temp}&#8451;<br/>
-                {munroWeather.list[6].weather[0].description}<br/>
-                {(munroWeather.list[8].dt_txt).slice(-8, -3)}- {munroWeather.list[8].main.temp}&#8451;<br/>
-                {munroWeather.list[8].weather[0].description}<br/>
-
+                <ul>
+                {weatherInfo}
+                </ul>
             </div>
             </div>
             </>
--- src/containers/MunroContainer.js
+++ src/containers/MunroContainer.js
@@ -7,8 +7,8 @@ const MunroContainer = function (){
 
     const [allMunros, setAllMunros] = useState([]);
     const [selectedMunro, setSelectedMunro] = useState(null);
-    const [filter, setFilter] = useState('');
     const [munroWeather, setMunroWeather] = useState(null);
+    const [filter, setFilter] = useState('');
 
     useEffect(() => {
         getAllMunros()
@@ -24,8 +24,30 @@ const MunroContainer = function (){
         setSelectedMunro(munro);
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${munro.latlng_lat}&lon=${munro.latlng_lng}&appid=566e1e687d708fd7e5c3ee6c8325feca&units=metric`)
         .then(res => res.json())
-        .then(weather => setMunroWeather(weather))
+        .then(weather => weatherDetails(weather))
     };
+
+    const weatherDetails = (munroWeather) => {
+
+        const beautifyDate = (date) => {
+            let month = date.slice(-14, -12)
+            let day = date.slice(-11, -9)
+            let time = date.slice(-8, -3)
+    
+            return (`${day}/${month} ${time}`)
+        }
+
+        let details = [];
+        for (let i = 0; i < 9; i += 2) {
+            details.push({
+                date: beautifyDate(munroWeather.list[i].dt_txt),
+                temp: (munroWeather.list[i].main.temp).toFixed(1),
+                description: ((munroWeather.list[i].weather[0].description)[0].toUpperCase() + (munroWeather.list[i].weather[0].description).substring(1))
+            })
+        }
+        setMunroWeather(details);
+
+    }
 
     return (
         <>
@@ -33,7 +55,7 @@ const MunroContainer = function (){
         <FilterBox setFilter={setFilter}/>
         <div className='munro-container'>
             <MunroList allMunros = {allMunros} munroSelected = {munroSelected} filter = {filter}/>
-            <MunroDetails selectedMunro = {selectedMunro} munroWeather = {munroWeather}/>
+            <MunroDetails selectedMunro = {selectedMunro} munroWeather={munroWeather}/>
         </div>
         </>
     );
