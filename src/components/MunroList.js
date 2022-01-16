3ad57c554af09bd811ed73cef5ed5ff8f380cd0e
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-16 16:06:06 +0000

    installed react-leaflet and leaflet to attempt map render

--- src/components/MunroList.js
+++ src/components/MunroList.js
@@ -4,9 +4,12 @@ import Munro from './Munro';
 const MunroList = function ({allMunros, munroSelected, checkFilter}){
 
     const MunroItems = allMunros.map((munro, index) => {
+        
         if (checkFilter(munro)) {
         return <Munro munro = {munro} key = {index} munroSelected = {munroSelected}/>
-    }})
+        }
+        return null
+    })
 
     return(
         <div className='munro-list'>
